50c6cf7e38610c7dded848ca6866048a440660ce
AndruxKilller7 <cristianandruortegavargas@hotmail.com>
2023-12-07 07:26:19 -0400

    Merkadito v1

---
+++ js/productos.js
@@ -0,0 +1,12 @@
+class Productos{
+    constructor(nombre,precio,coloresD,descripcion,categoria,estado){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.coloresD=coloresD;
+        this.descripcion= descripcion;
+        this.categoria=categoria;
+        this.estado= estado;
+    }
+}
+
+
